638b1d49e8d3725db2dc5cbaeae9771c4133cfe5
Matheus Mota <matheussilvamartins1714@gmail.com>
2025-10-06 12:57:52 -0300

    feat: define TypedDicts for academic work and simulation workflow in default_types.py; refactor ml_worker to use MLSimulationWorkflow

---
+++ src/default_types.py
@@ -0,0 +1,71 @@
+from typing import (
+  TypedDict,
+  List,
+  Dict,
+  NewType,
+)
+from datetime import datetime
+
+Id = NewType("Id", str)
+
+class AcademicWork(TypedDict):
+  id: str
+  title: str
+  abstract: str
+  keywords: List[str]
+  included: bool
+  db_source: str
+  created_at: datetime
+  updated_at: datetime
+
+class InputData(TypedDict):
+  title: str
+  keywords: List[str]
+  keywords: List[str]
+
+class GroundTruth(TypedDict):
+  included: bool
+
+class DataItem(TypedDict):
+  id: str
+  input: InputData
+  ground_truth: GroundTruth
+
+class PredictionIn(TypedDict):
+  data: List[DataItem]
+
+class PredictionOut(TypedDict):
+  predicted_labels: Dict[Id, bool]
+
+class EvaluationData(TypedDict):
+  true_positives: int
+  false_positives: int
+  true_negatives: int
+  false_negatives: int
+  total_positives: int
+  total_negatives: int
+  precision: float
+  recall: float
+  f2_score: float
+  wss95: float
+
+class SimulationConfig(TypedDict):
+  classification_strategy: str
+  tokenizer_strategy: str
+  version: str
+  dataset_version: str
+
+class SimulationRun(TypedDict):
+  id: str
+  input: PredictionIn
+  output: PredictionOut
+  evaluation: EvaluationData
+  config: SimulationConfig
+  created_at: datetime
+  updated_at: datetime
+
+class MLSimulationWorkflowIn(TypedDict):
+  ...
+
+class MLSimulationWorkflowOut(TypedDict):
+  ...
--- src/workers/ml_worker.py
+++ src/workers/ml_worker.py
@@ -1,35 +1,14 @@
-import os
-import asyncio
 from enum import Enum
-from dotenv import load_dotenv
+from temporalio.worker import UnsandboxedWorkflowRunner
 
-from temporalio.worker import Worker, UnsandboxedWorkflowRunner
-from temporalio.client import Client
-
-load_dotenv()
+from src.workflows.ml_simulation_workflow import MLSimulationWorkflow
 
 class WorflowTaskQueue(Enum):
     ML_TASK_QUEUE = "ml-task-queue"
 
-async def main():
-    try:
-        client = await Client.connect(os.environ.get("TEMPORAL_CONNECT"))
-
-        worker = Worker(
-            client,
-            task_queue=WorflowTaskQueue.ML_TASK_QUEUE.value,
-            workflows=[],
-            activities=[],
-            workflow_runner=UnsandboxedWorkflowRunner(),
-        )
-
-        print("Workers runnig")
-
-        await worker.run()
-    except Exception as e:
-        print(str(e))
-    finally:
-        print("Finish workers")
-
-if __name__ == "__main__":
-    asyncio.run(main())
+ml_worker = {
+    "workflows": [MLSimulationWorkflow],
+    "activities": [],
+    "workflow_runner": UnsandboxedWorkflowRunner(),
+    "task_queue": WorflowTaskQueue.ML_TASK_QUEUE.value,
+}
