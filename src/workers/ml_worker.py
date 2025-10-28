0176ad2f1b2cc0664c2a6854d032d254cb85af67
Matheus Mota <matheussilvamartins1714@gmail.com>
2025-10-27 23:52:09 -0300

    refactor: update ml_worker to include new SVM experiment workflow and activities, while removing obsolete workflows and activities for improved clarity and functionality

--- src/workers/ml_worker.py
+++ src/workers/ml_worker.py
@@ -1,42 +1,32 @@
-from enum import Enum
 from temporalio.worker import UnsandboxedWorkflowRunner
 
 from src.workflows.data_preprocessing_workflow import DataPreprocessingWorkflow
-from src.workflows.experiment_workflow import ExperimentWorkflow
-from src.workflows.tokenizer_shared_workflow import TokenizeSharedWorkflow
-from src.workflows.simulate_model_workflow import SimulateModelWorkflow
+from src.workflows.experiment_svm_with_glove_and_tfidf_workflow import ExperimentSVMWithGloveAndTFIDFWorkflow
 
 from src.activities.process_files_activity import process_files_activity
 from src.activities.merge_processed_files_activity import merge_processed_files_activity
+from src.activities.load_glove_embeddings_activity import load_glove_embeddings_activity
+from src.activities.run_experiment_svm_with_glove_and_tfidf_activity import run_experiment_svm_with_glove_and_tfidf_activity
 from src.activities.prepare_data_for_experiment_activity import prepare_data_for_experiment_activity
-from src.activities.train_model_activity import (
-    train_model_activity,
-    train_svm_activity,
-    train_random_forest_activity
-)
-from src.activities.validate_model_activity import validate_model_activity
-from src.activities.run_production_inference_activity import run_production_inference_activity
-from src.activities.aggregate_results_activity import aggregate_results_activity
+from src.activities.tokenizer_activity import tokenizer_activity
+from src.activities.split_data_activity import split_data_activity
+
 
 from constants import WorflowTaskQueue
 
 ml_worker = {
     "workflows": [
         DataPreprocessingWorkflow,
-        ExperimentWorkflow,
-        TokenizeSharedWorkflow,
-        SimulateModelWorkflow,
+        ExperimentSVMWithGloveAndTFIDFWorkflow,
     ],
     "activities": [
         process_files_activity,
         merge_processed_files_activity,
+        load_glove_embeddings_activity,
         prepare_data_for_experiment_activity,
-        train_model_activity,
-        train_svm_activity,
-        train_random_forest_activity,
-        validate_model_activity,
-        run_production_inference_activity,
-        aggregate_results_activity,
+        run_experiment_svm_with_glove_and_tfidf_activity,
+        tokenizer_activity,
+        split_data_activity,
     ],
     "workflow_runner": UnsandboxedWorkflowRunner(),
     "task_queue": WorflowTaskQueue.ML_TASK_QUEUE.value,
